466f987dc3871c36c4e880e4292ddf5b8c128ec8
Vjsvieira <45633594+Vjsvieira@users.noreply.github.com>
2019-01-07 03:17:49 +0000

    Add files via upload

--- DEMONCompiler.ml
+++ DEMONCompiler.ml
@@ -1,6 +1,6 @@
 open Format 
-open mipsDEMONLib
 open DEMONAst
+open mipsDEMONLib
 
 exception CompException of string
 
@@ -18,31 +18,33 @@ let compile_expr = function
             |_ -> nop
     end
     |Var s ->nop
-    |Binop op->
-    begin
+    |Binop op ( op,e1,e2 )->
+      let first = compile_expr e1 in
+      let second = compile_expr e2 in
+      begin
         match op with   (*verificar*)
-            Plus ->li v0 5 ++ compile_expr e1 ++ move t0 v0 ++ li v0 5 ++ move t1 v0 ++ add t2 t0 t1 ++ move a0 t2 ++ li v0 1 ++ syscall
-            |Minus ->li v0 5 ++ compile_expr e1 ++ move t0 v0 ++ li v0 5 ++ move t1 v0 ++ sub t2 t0 t1 ++ move a0 t2 ++ li v0 1 ++ syscall
-            |Times ->li v0 5 ++ compile_expr e1 ++ move t0 v0 ++ li v0 5 ++ move t1 v0 ++ mult t2 t0 t1 ++ move a0 t2 ++ li v0 1 ++ syscall
-            |Div ->li v0 5 ++ compile_expr e1 ++ move t0 v0 ++ li v0 5 ++ move t1 v0 ++ div t2 t0 t1 ++ move a0 t2 ++ li v0 1 ++ syscall
-            |Smaller ->nop
-            |Larger ->nop
-            |Lequal ->nop
-            |Sequal ->nop
-            |Equals ->nop
-            |Notequal ->nop
-    end
-    |Unop binop e->nop
-    |Letin str e1 e2->nop
+          Plus -> first ++ move t1 t0 ++ second ++ move t2 t0 ++ add t0 t1 oreg t2 
+          |Minus -> first ++ move t1 t0 ++ second ++ move t2 t0 ++ sub t0 t1 oreg t2 
+          |Times -> first ++ move t3 t0 ++ second ++ move t4 t0 ++ mult t0 t3 oreg t4 
+          |Div -> first ++ move t3 t0 ++ second ++ move t4 t0 ++ div t1 t2 ++ mflo t0
+          |Smaller -> first ++ move t1 t0 ++ second ++ move t2 t0 ++ slt t0 t0 oreg t1
+          |Larger -> first ++ move t1 t0 ++ second ++ move t2 t0 ++ sgt t0 t0 oreg t1
+          |Lequal -> first ++ move t1 t0 ++ second ++ move t2 t0 ++ sle t0 t0 oreg t1
+          |Sequal -> first ++ move t1 t0 ++ second ++ move t2 t0 ++ sge t0 t0 oreg t1
+          |Equals -> first ++ move t1 t0 ++ second ++ move t2 t0 ++ beq t0 t0 oreg t1
+          |Notequal -> first ++ move t1 t0 ++ second ++ move t2 t0 ++ bne t0 t0 oreg t1
+      end
+    |Unop ( binop, e )->nop
+    |Letin ( str, e1, e2 )->nop
     | _ -> nop
 
 let compile_instr = function
-    Set str e ->nop
-    |Print e -> li v0 1 ++ compile_expr e ++ move a0 t0 ++ syscall
-    |If e stmtl ->nop
-    |IfElse e stmtl1 stmtl2 ->nop
-    |While e stmtl ->nop
-    |For str e1 e2 stmtl ->nop
+    Set ( str, e ) ->nop
+    |Print ( e ) -> li v0 1 ++ compile_expr e ++ move a0 t0 ++ syscall
+    |If ( e, stmtl ) ->nop
+    |IfElse ( e, stmtl1, stmtl2 )  ->nop
+    |While ( e, stmtl ) ->nop
+    |For ( str, e1, e2, stmtl ) ->nop
     | _ -> nop
 
 
